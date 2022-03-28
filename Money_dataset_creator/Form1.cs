50bbc8e2b9601e9cd96a6bd84206a1ef158d72b5
unknown <mick.zuelch@gmx.de>
2022-03-28 15:44:00 +0200

    Added rotation

--- Money_dataset_creator/Form1.cs
+++ Money_dataset_creator/Form1.cs
@@ -17,10 +17,11 @@ namespace dataset_creator
     {
         string name;
 
-        ProgressBar pb = new ProgressBar();
         TextBox numberTestSamples = new TextBox();
         TextBox sampleNames = new TextBox();
         Button start = new Button();
+
+        Button rotate = new Button();
         public Form1()
         {
             InitializeComponent();
@@ -36,15 +37,17 @@ namespace dataset_creator
             numberTestSamples.Text = "16";
             this.Controls.Add(numberTestSamples);
 
-            pb.Size = new Size(this.ClientSize.Width, 30);
-            pb.Location = new Point(0, 70);
-            this.Controls.Add(pb);
-
             start.Size = new Size(100, 30);
             start.Location = new Point(this.ClientSize.Width - start.Width - 10, this.ClientSize.Height - start.Height - 10);
             start.Text = "create samples";
             start.Click += createSamples;
             this.Controls.Add(start);
+
+            rotate.Size = new Size(100, 30);
+            rotate.Location = new Point(this.ClientSize.Width - start.Width - 10, this.ClientSize.Height - start.Height - 110);
+            rotate.Text = "Rotate samples";
+            rotate.Click += flipPictures;
+            this.Controls.Add(rotate);
 
             //createNewSet("test",1);
         }
@@ -69,14 +72,177 @@ namespace dataset_creator
             int samples = Convert.ToInt32(numberTestSamples.Text);
             int numCores = Environment.ProcessorCount;
 
-            pb.Maximum = samples;
-            pb.Value = 0;
-            pb.Step = samples / numCores;
-
             for (int i = 0; i < numCores; i++)
             {
                 Thread t = new Thread(new ThreadStart(PictureCreate));
                 t.Start();
+            }
+
+        }
+
+        private void flipPictures(object sender, EventArgs e)
+        {
+            int imWidth = 0;
+            int imHeight = 0;
+            int counter = 0;
+            string sorucePath = @"C:\Users\Mick\Desktop\geldbilder\Reale_copy";
+            string storePath = @"C:\Users\Mick\Desktop\geldbilder\real_rotaed";
+            string[] fileNames = Directory.GetFiles(sorucePath, "*");
+            List<string> allPicturesPath = new List<string>();
+            List<string> allTextPath = new List<string>();
+
+            foreach (string file in fileNames)
+            {
+                if (file.EndsWith(".jpg"))
+                {
+                    allPicturesPath.Add(file);
+                }
+                else if (file.EndsWith(".txt"))
+                {
+                    allTextPath.Add(file);
+                }
+            }
+               
+
+            foreach (string pictureToRotate in allPicturesPath)
+            {
+                Bitmap bm = new Bitmap(pictureToRotate);
+                imWidth = bm.Width;
+                imHeight = bm.Height;
+                int tmpHeight = bm.Width;
+                int tmpWidth = bm.Height;
+                //int tmpHeight = bm.Height;
+                //int tmpWidth = bm.Width;
+                int rotation = 90;
+                string saveNamePrefix = pictureToRotate.Remove(0, sorucePath.Length);
+                string[] splittedName = saveNamePrefix.Split('.');
+                string[] lines = File.ReadAllLines(allTextPath[counter]);
+                string[] allFileNames = new string[4];
+                string[] allTextFileNames = new string[4];
+                allFileNames[0] = pictureToRotate;
+                allTextFileNames[0] = allTextPath[counter];
+                Random rnd = new Random();
+                for (int i = 0; i < 3; i++)
+                {
+                    string[] writeLines = new string[lines.Length];
+                    Bitmap tmpBm = new Bitmap(tmpWidth, tmpHeight);
+                    using (Graphics g = Graphics.FromImage(tmpBm))
+                    {
+                        g.RotateTransform(rotation);
+                        if (rotation == 90)
+                        {
+                            g.DrawImage(bm, new Point(0, -tmpWidth));
+                            //g.DrawImage(bm, new Point(0, 0));
+                            for (int j = 0; j < writeLines.Length; j++)
+                            {
+                                string[] splitedLine = lines[j].Split(' ');
+                                float xpos = float.Parse(splitedLine[1]) / 1000000;
+                                float ypos = float.Parse(splitedLine[2]) / 1000000;
+
+                                ypos = 1.0f - ypos;
+                                writeLines[j] += splitedLine[0] + " " + ypos + " " + xpos + " " + splitedLine[4] + " " + splitedLine[3];
+                                writeLines[j] = writeLines[j].Replace(',', '.');
+                            }
+                        }
+                        else if (rotation == 180)
+                        {
+                            g.DrawImage(bm, new Point(-tmpWidth, -tmpHeight));
+                            for (int j = 0; j < writeLines.Length; j++)
+                            {
+                                string[] splitedLine = lines[j].Split(' ');
+                                float xpos = float.Parse(splitedLine[1]) / 1000000;
+                                float ypos = float.Parse(splitedLine[2]) / 1000000;
+
+                                xpos = 1.0f - xpos;
+                                ypos = 1.0f - ypos;
+                                writeLines[j] += splitedLine[0] + " " + xpos + " " + ypos + " " + splitedLine[3] + " " + splitedLine[4];
+                                writeLines[j] = writeLines[j].Replace(',', '.');
+                            }
+                        }
+                        else if (rotation == 270)
+                        {
+                            g.DrawImage(bm, new Point(-tmpHeight, 0));
+                            for (int j = 0; j < writeLines.Length; j++)
+                            {
+                                string[] splitedLine = lines[j].Split(' ');
+                                float xpos = float.Parse(splitedLine[1]) / 1000000;
+                                float ypos = float.Parse(splitedLine[2]) / 1000000;
+
+                                xpos = 1.0f - xpos;
+                                //ypos = 1.0f - ypos;
+                                writeLines[j] += splitedLine[0] + " " + ypos + " " + xpos + " " + splitedLine[4] + " " + splitedLine[3];
+                                writeLines[j] = writeLines[j].Replace(',', '.');
+                            }
+                        }
+                    }
+                    tmpBm.Save(storePath + splittedName[0] + "_rotatet_" + rotation + ".jpg");
+                    allFileNames[i + 1] = storePath + splittedName[0] + "_rotatet_" + rotation + ".jpg";
+                    //tmpBm.Save(storePath + splittedName[0] + ".jpg");
+                    for (int j = 0; j < writeLines.Length; j++)
+                    {
+                        File.AppendAllText(storePath + splittedName[0] + "_rotatet_" + rotation + ".txt", writeLines[j] + "\n");
+                    }
+                    allTextFileNames[i + 1] = storePath + splittedName[0] + "_rotatet_" + rotation + ".txt";
+                    rotation += 90;
+                    int tmpSave = tmpHeight;
+                    tmpHeight = tmpWidth;
+                    tmpWidth = tmpSave;
+                }
+                counter++;
+
+                int addFeaturesToPicture = rnd.Next(0, 4);
+                if(addFeaturesToPicture == 3)
+                {
+                    int numEffects = rnd.Next(1, 10);
+                    for (int i = 0; i < numEffects; i++)
+                    {
+                        int selectPicture = rnd.Next(0, allFileNames.Length);
+                        int effect = rnd.Next(0, 3);
+                        Bitmap tmpBm = new Bitmap(allFileNames[selectPicture]);
+                        string suffix = "effect_" + i;
+                        using (Graphics g = Graphics.FromImage(tmpBm))
+                        {
+                            switch(effect)
+                            {
+                                case 0:
+                                    int alpha = rnd.Next(50, 200);
+                                    Brush fill = new SolidBrush(Color.FromArgb(alpha, 0, 0, 0));
+                                    g.FillRectangle(fill, new Rectangle(0, 0, tmpBm.Width, tmpBm.Height));
+                                    break;
+                                case 1:
+                                    int alphaR = rnd.Next(50, 200);
+                                    int r = rnd.Next(0, 256);
+                                    int G = rnd.Next(0, 256);
+                                    int b = rnd.Next(0, 256);
+                                    Brush fillRnd = new SolidBrush(Color.FromArgb(alphaR, r, G, b));
+                                    g.FillRectangle(fillRnd, new Rectangle(0, 0, tmpBm.Width, tmpBm.Height));
+                                    break;
+                                case 2:
+                                    int numRects = rnd.Next(1, 25);
+                                    int rectWidth = tmpBm.Width / (2 * numRects);
+                                    int rectHeight = tmpBm.Height / (2 * numRects);
+                                    Brush black = new SolidBrush(Color.Black);
+                                    for (int k = 0; k < numRects; k++)
+                                    {
+                                        for (int j = 0; j < numRects; j++)
+                                        {
+                                            g.FillRectangle(black, new Rectangle(rectWidth * k * 2, rectWidth * j * 2, rectWidth, rectHeight));
+                                        }
+                                    }
+                                    break;
+                            }
+                        }
+                        string[] tmpLines = File.ReadAllLines(allTextFileNames[selectPicture]);
+                        string textSavePath = allTextFileNames[selectPicture].Remove(allTextFileNames[selectPicture].Length - 4, 4);
+                        string pictrueSavePath = allFileNames[selectPicture].Remove(allFileNames[selectPicture].Length - 4, 4);
+                        tmpBm.Save(pictrueSavePath + suffix + ".jpg");
+                        for (int j = 0; j < tmpLines.Length; j++)
+                        {
+                            File.AppendAllText(pictrueSavePath + suffix + ".txt", tmpLines[j] + "\n");
+                        }
+                    }
+                }
+
             }
 
         }
@@ -188,8 +354,8 @@ namespace dataset_creator
 
                     while (randomX < 0 && randomY < 0)
                     {
-                        randomX = rnd.Next(0, imWidth - objects[k].Width);
-                        randomY = rnd.Next(0, imHeight - objects[k].Height);
+                        randomX = rnd.Next(0, imWidth);
+                        randomY = rnd.Next(0, imHeight);
                     }
 
                     posX.Add(randomX);
@@ -211,23 +377,23 @@ namespace dataset_creator
                 {
                     int alpha = rnd.Next(125, 200);
                     Brush shadowbox = null;
-                    int color= rnd.Next(0, 5);
+                    int color = rnd.Next(0, 4);
                     switch (color)
                     {
                         case (0):
-                            shadowbox = new SolidBrush(Color.FromArgb( alpha,145,128,0));
+                            shadowbox = new SolidBrush(Color.FromArgb(alpha, 145, 128, 0));
                             break;
                         case (1):
-                            shadowbox = new SolidBrush(Color.FromArgb(alpha, 20,0,145));
+                            shadowbox = new SolidBrush(Color.FromArgb(alpha, 20, 0, 145));
                             break;
-                        case (3):
+                        case (2):
                             shadowbox = new SolidBrush(Color.FromArgb(alpha, 0, 0, 0));
                             break;
-                        case (4):
+                        case (3):
                             shadowbox = new SolidBrush(Color.FromArgb(alpha, 204, 27, 0));
                             break;
                     }
-                    g.FillRectangle(shadowbox, new Rectangle(0,0,bm.Width,bm.Height));
+                    g.FillRectangle(shadowbox, new Rectangle(0, 0, bm.Width, bm.Height));
                 }
             }
             if (generateBBoxFiles)
